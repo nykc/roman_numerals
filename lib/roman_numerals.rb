751ecdd3aa8e4cf8f74d6963872f0cd33788812b
Derek Ackley <dackley@nykc.net>
2016-09-05 11:46:31 -0400

    roman numeral conversion

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,12 +1,30 @@
 class RomanNumeral
 
+    NUMERALS = [
+        ["M",1000],
+        ["CM",900],
+        ["D",500],
+        ["CD",400],
+        ["C",100],
+        ["XC",90],
+        ["L",50],
+        ["XL",40],
+        ["X",10],
+        ["IX",9],
+        ["V",5],
+        ["IV",4],
+        ["I",1]
+    ]
+
     def convert(num)
-        return "IV" if num == 4
 
         result = ""
-        while num >= 1
-            result += "I"
-            num -= 1
+
+        NUMERALS.each do | roman, arabic|
+            while num >= arabic
+                result += roman
+                num -= arabic
+            end
         end
 
         result
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -6,13 +6,31 @@ describe "Roman Numerals" do
         @roman = RomanNumeral.new
     end
 
-    context "convert base symbols (I, V, X)" do
+    context "convert base symbols (I, V, X, L, C, D, M)" do
         it "should convert number 1 to numeral I" do
             @roman.convert(1).should == "I"
         end
+        it "should convert number 5 to numeral V" do
+            @roman.convert(5).should == "V"
+        end
+        it "should convert number 10 to numeral X" do
+            @roman.convert(10).should == "X"
+        end
+        it "should convert number 50 to numeral L" do
+            @roman.convert(50).should == "L"
+        end
+        it "should convert number 100 to numeral C" do
+            @roman.convert(100).should == "C"
+        end
+        it "should convert number 500 to numeral D" do
+            @roman.convert(500).should == "D"
+        end
+        it "should convert number 1000 to numeral M" do
+            @roman.convert(1000).should == "M"
+        end
     end
 
-    context "convert number additions" do
+    context "convert additions" do
 
         it "should convert number 2 to numeral II" do
             @roman.convert(2).should == "II"
@@ -22,11 +40,48 @@ describe "Roman Numerals" do
             @roman.convert(3).should == "III"
         end
 
+        it "should convert number 8 to numeral VIII" do
+            @roman.convert(8).should == "VIII"
+        end
+
+        it "should convert number 20 to numeral XX" do
+            @roman.convert(20).should == "XX"
+        end
+
+        it "should convert number 84 to numeral LXXXIV" do
+            @roman.convert(84).should == "LXXXIV"
+        end
+        it "should convert number 1066 to numeral MLXVI" do
+            @roman.convert(1066).should == "MLXVI"
+        end
+        it "should convert number 1989 to numeral MCMLXXXIX" do
+            @roman.convert(1989).should == "MCMLXXXIX"
+        end
     end
 
     context "convert subtractions" do
         it "should convert number 4 to numeral IV" do
             @roman.convert(4).should == "IV"
+        end
+
+        it "should convert number 9 to numeral IX" do
+            @roman.convert(9).should == "IX"
+        end
+
+        it "should convert number 40 to numeral XL" do
+            @roman.convert(40).should == "XL"
+        end
+
+        it "should convert number 90 to numeral XC" do
+            @roman.convert(90).should == "XC"
+        end
+
+        it "should convert number 400 to numeral CD" do
+            @roman.convert(400).should == "CD"
+        end
+
+        it "should convert number 900 to numeral CM" do
+            @roman.convert(900).should == "CM"
         end
     end
 
