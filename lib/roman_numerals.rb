d0e0c02d74613953de34428e9ff5b75cfa490d60
Derek Ackley <dackley@nykc.net>
2016-09-05 09:28:30 -0400

    add numbers 1-3 forgot to add rb file

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,6 +1,12 @@
 class RomanNumeral
 
-    def convert(number)
-        "I" * number
+    def convert(num)
+        result = ""
+        while num >= 1
+            result += "I"
+            num -= 1
+        end
+
+        result
     end
 end
